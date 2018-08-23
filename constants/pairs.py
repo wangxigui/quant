8ad9a82c685d68714b1412463625260ec23a667f
wangxigui <wangxigui@gf.com.cn>
2018-08-22 22:30:01 -0700

    add pairs

---
+++ constants/pairs.py
@@ -0,0 +1,17 @@
+#!/use/bin/env python
+# -*- coding: utf8 -*-
+
+''' 银行 '''
+1th = ['002142.XSHE', '600036.XSHG']
+
+
+''' 白酒 '''
+1th = ['603369.XSHG', '603589.XSHG']  # 5stars
+2th = ['600809.XSHG', '600779.XSHG']  # 5stars
+3th = ['000596.XSHE', '603589.XSHG']  # 4.5stars
+4th = ['002304.XSHE', '600519.XSHG']  # 5.1 stars
+5th = ['600809.XSHG', '600519.XSHG']  # 4 stars
+6th = ['000858.XSHE', '000568.XSHE']  # 5 starts
+7th = ['002304.XSHE', '600779.XSHG'] # 5.5 starts
+
+3th = ['603919.XSHG', '603198.XSHG']
